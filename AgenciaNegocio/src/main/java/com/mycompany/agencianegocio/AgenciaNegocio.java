e9b4ff5b6ea7e6e286aa3d593df437d14f3d5438
Carlos H Sánchez <carloshiramsanchez@gmail.com>
2024-03-19 14:33:25 -0700

    AgenciaNegocio y AgenciaPersistencia
    creacion de los demas proyectos y avance en  IndexFrame

---
+++ AgenciaNegocio/src/main/java/com/mycompany/agencianegocio/AgenciaNegocio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.agencianegocio;
+
+/**
+ *
+ * @author carlo
+ */
+public class AgenciaNegocio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ AgenciaPersistencia/src/main/java/com/mycompany/agenciapersistencia/AgenciaPersistencia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.agenciapersistencia;
+
+/**
+ *
+ * @author carlo
+ */
+public class AgenciaPersistencia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
